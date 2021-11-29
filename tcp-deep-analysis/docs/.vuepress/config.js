f115e7d61ca07f350705b1df62c02a28330c0e73
wangzhen <wangzhen05@wondersgroup.com>
2021-11-29 14:10:19 +0800

    feat:TCP 协议：从原理到实战

---
+++ tcp-deep-analysis/docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+const sidebarArr = require('./config/createSidebar.js');
+module.exports = {
+  title: '技术文档',
+  description: '技术文档',
+  base: '/book-docs/tcp-deep-analysis/',
+  port: '10100',
+  markdown: {
+    lineNumbers: true, // 代码块显示行号
+  },
+  themeConfig: {
+    defaultTheme: 'light',
+    sidebar: sidebarArr,
+  },
+};
--- vue-depth-analysis/docs/.vuepress/config.js
+++ vue-depth-analysis/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const sidebarArr = require('./config/createSidebar.js');
 module.exports = {
   title: '技术文档',
   description: '技术文档',
-  base: '/book-docs/vue-depth-analysis',
+  base: '/book-docs/vue-depth-analysis/',
   port: '10100',
   markdown: {
     lineNumbers: true, // 代码块显示行号
